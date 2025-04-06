bfeeca206969d49c3e2935a0aa73938685a891a6
phamdinhthien <phamdinhthien99@gmail.com>
2025-04-06 18:34:23 +0700

    1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import LuckySheet from './components/LuckySheet.vue'
 </script>
 
 <template>
   <LuckySheet />
-
 </template>
 
 <style>
